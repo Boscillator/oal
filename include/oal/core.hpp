46636df2bf11ef7c108f9b4856540f42bec7e883
Fred Buchanan <oscillator.b@gmail.com>
2025-04-13 10:42:59 -0400

    Added Eigen dependency

--- include/oal/core.hpp
+++ include/oal/core.hpp
@@ -1,3 +1,12 @@
+#ifndef OAL_CORE_HPP_
+#define OAL_CORE_HPP_
+
+#include <Eigen/Core>
 namespace oal {
+  using Vec3 = Eigen::Vector3d;
+  using Mat3 = Eigen::Matrix3d;
+
   void hello();
 }
+
+#endif
