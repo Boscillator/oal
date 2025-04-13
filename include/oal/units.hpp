4971d16c5f3403ae6afb3dd7dc7ad5cc5b7e288e
Fred Buchanan <oscillator.b@gmail.com>
2025-04-13 18:44:21 -0400

    Added docstring and python docs

--- include/oal/units.hpp
+++ include/oal/units.hpp
@@ -6,14 +6,34 @@
 namespace oal {
 namespace units {
 
+// @brief Unit of measure representing an angle.
 enum AngleUnit {
+  // @brief Angle in degrees (360 degrees per circle).
   DEGREES,
+  // @brief Angle in radians (2 * pi radians per circle).
   RADIANS
 };
 
+// @brief Convert an angle to radians.
+// @param angle_deg Angle in degrees.
+// @return Angle in radians.
 OAL_EXPORT double to_radians(const double angle_deg);
+
+// @brief Convert an angle to radians.
+// @param angle Angle to convert in `units`.
+// @param units Units to convert from.
+// @return Angle in radians.
 OAL_EXPORT double to_radians(const double angle, const AngleUnit units);
+
+// @brief Convert an angle to degrees.
+// @param angle_radians Angle in radians.
+// @return Angle in degrees.
 OAL_EXPORT double to_degrees(const double angle_radians);
+
+// @brief Convert an angle to degrees.
+// @param angle Angle to convert in `units`.
+// @param units Units to convert from.
+// @return Angle in degrees.
 OAL_EXPORT double to_degrees(const double angle, const AngleUnit units);
 
 }
--- src/python/units.cpp
+++ src/python/units.cpp
@@ -12,13 +12,14 @@ void register_units(py::module_& m) {
   py::enum_<oal::units::AngleUnit>(units, "AngleUnit")
     .value("RADIANS", oal::units::RADIANS)
     .value("DEGREES", oal::units::DEGREES)
-    .export_values();
+    .export_values()
+    .doc() = "Unit of measure representing an angle.";
 
   double (*to_degrees)(const double, const oal::units::AngleUnit) = &oal::units::to_degrees;  // Specify overload resolution
-  units.def("to_degrees", py::vectorize(to_degrees), "", py::arg("angle"), py::arg("units") = units::RADIANS);
+  units.def("to_degrees", py::vectorize(to_degrees), "Convert an angle to radians", py::arg("angle"), py::arg("units") = units::RADIANS);
 
   double (*to_radians)(const double, const oal::units::AngleUnit) = &oal::units::to_radians;  // Specify overload resolution
-  units.def("to_radians", py::vectorize(to_radians), "", py::arg("angle"), py::arg("units") = units::DEGREES);
+  units.def("to_radians", py::vectorize(to_radians), "Convert an angle to degrees", py::arg("angle"), py::arg("units") = units::DEGREES);
 }
 
 }
