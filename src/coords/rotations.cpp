7a3548116f56c825e4abc0a60904c36984f3963e
Fred Buchanan <oscillator.b@gmail.com>
2025-04-18 17:13:51 -0400

    Added rotate_y and rotate_z functions

--- src/coords/rotations.cpp
+++ src/coords/rotations.cpp
@@ -10,9 +10,17 @@ Mat3 rotate_x(const double angle_rad) {
 }
 
 
-// Mat3 rotate_y(const double angle_rad) { return Mat3(); }
+Mat3 rotate_y(const double angle_rad) { 
+  const double s_theta = std::sin(angle_rad);
+  const double c_theta = std::cos(angle_rad);
+  return (Mat3() << c_theta, 0, -s_theta, 0, 1, 0, s_theta, 0, c_theta).finished();
+}
 
-// Mat3 rotate_z(const double angle_rad) { return Mat3(); }
+Mat3 rotate_z(const double angle_rad) { 
+  const double s_theta = std::sin(angle_rad);
+  const double c_theta = std::cos(angle_rad);
+  return (Mat3() << c_theta, s_theta, 0, -s_theta, c_theta, 0, 0, 0, 1).finished();
+}
 
 // Mat3 dcm_from_eulers(const Vec3Ref eulers_rad) { 
 //   return Mat3();
--- tests/cpp/coords/test_single_axis_rotations.cpp
+++ tests/cpp/coords/test_single_axis_rotations.cpp
@@ -3,14 +3,40 @@
 #include "oal/coords/rotations.hpp"
 #include "../matchers.hpp"
 
-TEST_CASE("Test rotate_x", "[coords]") {
-  const oal::Vec3 point = {0, 0, 1};
+TEST_CASE("Test rotate_{axis} family functions", "[coords]") {
+  SECTION("rotate_x") {
+    // Rotate coordinate system by 90 degrees about the x-axis, counter clockwise
+    const oal::Vec3 point = {0, 0, 1};
+    
+    const oal::Vec3 new_point = oal::coords::rotate_x(oal::pi/2) * point; 
+
+    const oal::Vec3 expected_point {0, 1, 0};
+    const double tolerance = 1e-12;
+
+    REQUIRE_THAT(new_point, oal::test_support::MatrixWithinAbs(expected_point, tolerance));
+  }
+
+  SECTION("rotate_y") {
+    // Rotate coordinate system by 90 degrees counter clockwise about the y-axis
+    const oal::Vec3 point = {0, 0, 1};
+    
+    const oal::Vec3 new_point = oal::coords::rotate_y(oal::pi/2) * point; 
+
+    const oal::Vec3 expected_point {-1, 0, 0};
+    const double tolerance = 1e-12;
+
+    REQUIRE_THAT(new_point, oal::test_support::MatrixWithinAbs(expected_point, tolerance));
+  }
+
+  SECTION("rotate_z") {
+    // Rotating a point on the z-axis about the z-axis should result in the identity matrix
+    const oal::Vec3 point = {0, 0, 1};
   
-  // Rotate coordinate system by 90 degrees about the x-axis, counter clockwise
-  const oal::Vec3 new_point = oal::coords::rotate_x(oal::pi/2) * point; 
+    const oal::Vec3 new_point = oal::coords::rotate_z(oal::pi/2) * point; 
 
-  const oal::Vec3 expected_point {0, 1, 0};
-  const double tolerance = 1e-12;
+    const oal::Vec3 expected_point {0, 0, 1};
+    const double tolerance = 1e-12;
 
-  REQUIRE_THAT(new_point, oal::test_support::MatrixWithinAbs(expected_point, tolerance));
+    REQUIRE_THAT(new_point, oal::test_support::MatrixWithinAbs(expected_point, tolerance));
+  }
 }
